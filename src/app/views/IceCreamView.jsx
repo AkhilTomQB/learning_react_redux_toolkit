9792f18bc37efd210908911baf8b8571979b80ca
akhil.abraham <akhil.abraham@qburst.com>
2024-10-04 01:07:40 +0530

    hooks for react redux

--- src/app/views/IceCreamView.jsx
+++ src/app/views/IceCreamView.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
+import { useSelector,useDispatch } from 'react-redux'
+import { ordered,restocked } from '../../features/iceCream/iceCreamSlice'
 
 const IceCreamView = () => {
+  const numOfIceCreams=useSelector((state)=>state.iceCream.numOfIceCreams)
+  const dispatch= useDispatch()
   return (
     <div>
-        <h2>Number of iceCreams</h2>
-        <button>Order iceCream</button>
-        <button>Restock iceCream</button>
+        <h2>Number of iceCreams:{numOfIceCreams}</h2>
+        <button onClick={()=>dispatch(ordered())}>Order iceCream</button>
+        <button onClick={()=>dispatch(restocked(5))} >Restock iceCream</button>
     </div>
   )
 }
