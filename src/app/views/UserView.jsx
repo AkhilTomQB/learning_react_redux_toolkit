053ea6ae3c26042826b20dea1ab6a668743ee590
akhil.abraham <akhil.abraham@qburst.com>
2024-10-04 01:22:18 +0530

    redux react thunk

--- src/app/views/UserView.jsx
+++ src/app/views/UserView.jsx
@@ -1,9 +1,19 @@
-import React from "react";
-
+import React,{useEffect} from "react";
+import { useSelector,useDispatch } from "react-redux";
+import { fetchUsers } from "../../features/users/userSlice";
 const UserView = () => {
+  const user=useSelector(state=>state.user)
+  const dispatch=useDispatch()
+  useEffect(()=>{
+    dispatch(fetchUsers())
+  },[])
   return (
     <div>
+      
       <h2>List of users</h2>
+      {user.loading && <div>Loading....</div>}
+      {!user.loading && user.error? <div>Error: {user.error}</div>:""}
+      {!user.loading && user.users.length?(<ul>{user.users.map((user=>(<li key={user.id}>{user.name}</li>)))}</ul>):""}
     </div>
   );
 };
